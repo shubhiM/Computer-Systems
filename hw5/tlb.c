e0412026a4c31845cd7f0fa1c6b3b6e574a095b5
shubhiM <mittal.sh@husky.neu.edu>
2017-11-04 18:30:48 -0400

    Improved code to time the loops

--- hw5/tlb.c
+++ hw5/tlb.c
@@ -9,41 +9,38 @@
 
 #define PAGESIZE sysconf(_SC_PAGESIZE)
 
-// Array is complely in memory
-void experiment1(int trials, int pages){
-        struct timespec start, end;
-        int arraySize = (PAGESIZE/sizeof(int)) * pages;
-        printf("size of array %d\n", arraySize);
-        int a[arraySize];
+int a[1048576];
 
-        //Initialize array so that it gets into main
-        //Memory
-        for(int i=0; i<arraySize; i++) {
+void initialize(int a[], int size){
+        for(int i=0; i<size; i++) {
                 a[i] = 0;
         }
-
+}
+// Array is complely in memory
+void experiment(int trials, int pages){
+        struct timespec start, end;
         int jump = PAGESIZE/sizeof(int);
         double total = 0;
         double denom = 0.0;
+        clock_gettime(CLOCK_REALTIME, &start);
         for (int t=0; t<trials; t++) {
                 // Access each page once
                 for(int s=0; s<pages*jump; s+=jump) {
                         // 0 to 1023 elements of the array belong
                         // to the same page
-                        clock_gettime(CLOCK_REALTIME, &start);
                         a[s]+=1;
-                        clock_gettime(CLOCK_REALTIME, &end);
-                        unsigned long start_time = 1000000000*start.tv_sec + start.tv_nsec;
-                        unsigned long end_time = 1000000000*end.tv_sec + end.tv_nsec;
-                        unsigned long diff = end_time - start_time;
-                        total+= diff;
                         denom += 1.0;
-                        //printf("Trial %d, Time (ms) %lu, Stride: %d\n", t, diff, s);
                 }
         }
+        clock_gettime(CLOCK_REALTIME, &end);
+        unsigned long start_time = 1000000000*start.tv_sec + start.tv_nsec;
+        unsigned long end_time = 1000000000*end.tv_sec + end.tv_nsec;
+        unsigned long diff = end_time - start_time;
+        total+=diff;
         printf("Average access time(nsec)/page %lf for reading %d pages over %d trials\n",
                total/(denom), pages, trials);
 }
+
 int main(int argc, char const *argv[]) {
         /* code */
 
@@ -55,8 +52,11 @@ int main(int argc, char const *argv[]) {
                 exit(1);
         }
 
-        for(int i=1; i<=25; ) {
-                experiment1(10000000, i);
-                i = i + 2;
+        //initialize the global array
+        initialize(a, 1048576);
+
+        for(int i=2; i<=1024; ) {
+                experiment(100000000, i);
+                i = i*2;
         }
 }
