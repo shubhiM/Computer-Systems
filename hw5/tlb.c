11d6c5662e66fe42a3f83e2e121bef0f52a4347e
shubhiM <mittal.sh@husky.neu.edu>
2017-11-06 12:06:11 -0500

    Modified tlb.c

--- hw5/tlb.c
+++ hw5/tlb.c
@@ -44,7 +44,7 @@ void specCacheSize(int trials, int pages){
         int jump = PAGESIZE/sizeof(int);
         double total = 0;
         double denom = 0.0;
-        clock_gettime(CLOCK_REALTIME, &start);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
         for (int t=0; t<trials; t++) {
                 // Access each page once
                 for(int s=0; s<pages*jump; s+=jump) {
@@ -55,7 +55,7 @@ void specCacheSize(int trials, int pages){
                 }
 
         }
-        clock_gettime(CLOCK_REALTIME, &end);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
         unsigned long start_time = 1000000000*start.tv_sec + start.tv_nsec;
         unsigned long end_time = 1000000000*end.tv_sec + end.tv_nsec;
         unsigned long diff = end_time - start_time;
@@ -64,6 +64,23 @@ void specCacheSize(int trials, int pages){
                total/(denom), pages, trials);
 }
 
+
+void specAssociativity(){
+
+        // Cache full
+        for(int i=0; i<8192*2; i++) {
+                a[i]+=1;
+        }
+
+        for(int i=0; i<=8192*16; i=i+8192) {
+                getTimeToAccessIthElement(i);
+                printf("******************************************\n");
+                for(int j=0; j<=i; j=j+8192) {
+                        getTimeToAccessIthElement(j);
+                }
+                printf("******************************************\n");
+        }
+}
 
 void runCacheSizeSpec(){
         initialize(a, 1048576);
@@ -82,5 +99,6 @@ int main(int argc, char const *argv[]) {
         }
         runCacheSizeSpec();
         specCacheLineSize();
+        //specAssociativity();
 
 }
