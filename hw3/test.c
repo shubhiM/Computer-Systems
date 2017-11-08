c5c2ff0aae7cb32eb2dbc458940c7c75087614a7
shubhiM <mittal.sh@husky.neu.edu>
2017-11-07 19:39:15 -0500

    Free implementation testing complete

--- hw3/test.c
+++ hw3/test.c
@@ -15,14 +15,24 @@ struct MemoryNodeHeader {
         struct MemoryNodeHeader* nextRoot;
 };
 
-void allocation(unsigned int size, char c){
+void* allocation(unsigned int size, char c){
         void *mem_1 = malloc(size);
         struct MemoryNodeHeader* node_1 = (struct MemoryNodeHeader* ) mem_1 - 1;
         assert(node_1->dataSize >= size);
         assert(memset(mem_1, c, size-1) != NULL);
         memset(mem_1 + size-1, '\0', 1);
-        printf("%s\n", mem_1);
+        printf("%p %s\n", mem_1, mem_1);
+        return mem_1;
 }
+
+void testScenarioFree(){
+        void* ptr_1 = allocation(16, 'a');
+        void *ptr_2 = allocation(16, 'b');
+        free(ptr_2);
+        free(ptr_1);
+        allocation(16, 'c');
+}
+
 void testScenarioMalloc(){
         // Testing Heap Consistency for consequetive mallocs
         allocation(16, 'a');
@@ -35,5 +45,6 @@ void testScenarioMalloc(){
 int main(int argc, char **argv)
 {
         testScenarioMalloc();
+        testScenarioFree();
         return 0;
 }
