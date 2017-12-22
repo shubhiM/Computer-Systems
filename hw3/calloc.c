e87ab9c988001d21825e6cec33570c966c39a604
shubhi31 <mittal.sh@husky.neu.edu>
2017-12-22 17:07:00 +0530

    Restructing the repository and code clean

--- hw3/calloc.c
+++
@@ -1,9 +0,0 @@
-#include <sys/types.h>
-#include <string.h>
-#include "utils.h"
-
-void* calloc(size_t nmem, size_t size){
-        size_t totalSize = nmem * size;
-        void* ptr = allocateMemory(totalSize);
-        return memset(ptr, 0, totalSize);
-}
--- hw3/free.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-#include "utils.h"
-
-void free(void* ptr) {
-        freeWrapper(ptr);
-}
